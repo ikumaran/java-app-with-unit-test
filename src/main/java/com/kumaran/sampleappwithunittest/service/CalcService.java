7ecabd99de98c285971d1c664a834cadd2a17252
Kumaran Ilangovan <i.kumaran@giggso.com>
2022-07-27 13:22:16 +0530

    unit test updates

--- src/main/java/com/kumaran/sampleappwithunittest/service/CalcService.java
+++ src/main/java/com/kumaran/sampleappwithunittest/service/CalcService.java
@@ -2,16 +2,17 @@ package com.kumaran.sampleappwithunittest.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-
 @Service
 public class CalcService {
 
-    public int add(int... input) {
-        return Arrays.stream(input).sum();
+    public int add(Integer first, Integer second) {
+        return first + second;
     }
 
     public int divide(int first, int second) {
+        if (second == 0) {
+            throw new NumberFormatException("bad input for second parameter");
+        }
         return first / second;
     }
 }
--- src/test/java/com/kumaran/sampleappwithunittest/service/CalcServiceTest.java
+++ src/test/java/com/kumaran/sampleappwithunittest/service/CalcServiceTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.util.AssertionErrors;
 
+import javax.xml.ws.http.HTTPException;
+
 @SpringBootTest
 class CalcServiceTest {
 
@@ -19,10 +21,17 @@ class CalcServiceTest {
     }
 
     @Test
-    public void addTest_null() {
-        int sum = calcService.add();
+    public void addTest_lessThanZero() {
+        Integer sum = calcService.add(0, 100);
+        AssertionErrors.assertEquals("result not matching", 100, sum);
+    }
+
+    @Test
+    public void addTest_bothZero() {
+        Integer sum = calcService.add(0, 0);
         AssertionErrors.assertEquals("result not matching", 0, sum);
     }
+
     @Test
     public void divideTest() {
         int divide = calcService.divide(0, 1);
@@ -31,7 +40,12 @@ class CalcServiceTest {
 
     @Test
     public void divideTest_zeroDivider() {
-        Assertions.assertThrows(ArithmeticException.class, () -> calcService.divide(0, 0));
+        Assertions.assertThrows(NumberFormatException.class, () -> calcService.divide(0, 0));
     }
 
+//    @Test
+//    public void addTest_null() {
+//        int sum = calcService.add(null, null);
+//        AssertionErrors.assertEquals("result not matching", 0, sum);
+//    }
 }
