835ad32860484eb7f1c3af11f48393bf04ed8a73
Kumaran Ilangovan <i.kumaran@giggso.com>
2022-07-27 12:00:54 +0530

    Init version

---
+++ src/main/java/com/kumaran/sampleappwithunittest/SampleAppWithUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.kumaran.sampleappwithunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppWithUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppWithUnitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kumaran/sampleappwithunittest/SampleAppWithUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumaran.sampleappwithunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppWithUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
